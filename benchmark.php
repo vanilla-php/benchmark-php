d95d1eed81440990120416ef3ea4744322618668
odan <odan@users.noreply.github.com>
2015-08-16 19:57:06 +0200

    Refactored variable names

--- benchmark.php
+++ benchmark.php
@@ -8,44 +8,54 @@
  * - www.webdesign-informatik.de
  *
  * @author odan
- * @version 2014.02.23
  * @license MIT
- *
  */
 // -----------------------------------------------------------------------------
 // Setup
 // -----------------------------------------------------------------------------
 set_time_limit(120); // 2 minutes
 
-$arr_cfg = array();
+$options = array();
 
-// optional: mysql performance test
-//$arr_cfg['db.host'] = 'localhost';
-//$arr_cfg['db.user'] = 'root';
-//$arr_cfg['db.pw'] = '';
-//$arr_cfg['db.name'] = 'test';
+// Optional: mysql performance test
+//$options['db.host'] = '127.0.0.1';
+//$options['db.user'] = 'root';
+//$options['db.pw'] = '';
+//$options['db.name'] = 'test';
 // -----------------------------------------------------------------------------
 // Main
 // -----------------------------------------------------------------------------
 // check performance
-$arr_benchmark = test_benchmark($arr_cfg);
+$benchmarkResult = test_benchmark($options);
 
 // html output
 echo "<!DOCTYPE html>\n<html><head>\n";
-echo "<style>table {
-    font-family:Arial, Helvetica, sans-serif;
-    font-size:12px;
-    margin:0px;
-    padding: 0px;
-    border:#ccc 1px solid;
-    border-collapse:collapse;
-}
-td, th {
-    border:#ccc 1px solid;
-    vertical-align: top;
-}</style></head><body>";
+echo "<style>
+    table { 
+        color: #333; /* Lighten up font color */
+        font-family: Helvetica, Arial, sans-serif; /* Nicer font */
+        width: 640px; 
+        border-collapse: 
+        collapse; border-spacing: 0; 
+    }
 
-echo array_to_html($arr_benchmark);
+    td, th { 
+        border: 1px solid #CCC; height: 30px;
+    } /* Make cells a bit taller */
+
+    th {
+        background: #F3F3F3; /* Light grey background */
+        font-weight: bold; /* Make sure they're bold */
+    }
+
+    td {
+        background: #FAFAFA; /* Lighter grey background */
+    }
+    </style>
+    </head>
+    <body>";
+
+echo array_to_html($benchmarkResult);
 
 echo "\n</body></html>";
 exit;
@@ -54,140 +64,129 @@ exit;
 // Benchmark functions
 // -----------------------------------------------------------------------------
 
-function test_benchmark($arr_cfg)
+function test_benchmark($settings)
 {
+    $timeStart = microtime(true);
 
-    $time_start = microtime(true);
+    $result = array();
+    $result['version'] = '1.1';
+    $result['sysinfo']['time'] = date("Y-m-d H:i:s");
+    $result['sysinfo']['php_version'] = PHP_VERSION;
+    $result['sysinfo']['platform'] = PHP_OS;
+    $result['sysinfo']['server_name'] = $_SERVER['SERVER_NAME'];
+    $result['sysinfo']['server_addr'] = $_SERVER['SERVER_ADDR'];
 
-    $arr_return = array();
-    $arr_return['version'] = '1.1';
-    $arr_return['sysinfo']['time'] = date("Y-m-d H:i:s");
-    $arr_return['sysinfo']['php_version'] = PHP_VERSION;
-    $arr_return['sysinfo']['platform'] = PHP_OS;
-    $arr_return['sysinfo']['server_name'] = $_SERVER['SERVER_NAME'];
-    $arr_return['sysinfo']['server_addr'] = $_SERVER['SERVER_ADDR'];
-
-    test_math($arr_return);
-
-    test_string($arr_return);
-
-    test_loops($arr_return);
-
-    test_ifelse($arr_return);
-
-    if (isset($arr_cfg['db.host'])) {
-        test_mysql($arr_return, $arr_cfg);
+    test_math($result);
+    test_string($result);
+    test_loops($result);
+    test_ifelse($result);
+    if (isset($settings['db.host'])) {
+        test_mysql($result, $settings);
     }
 
-    $arr_return['total'] = timer_diff($time_start);
-
-    return $arr_return;
+    $result['total'] = timer_diff($timeStart);
+    return $result;
 }
 
-function test_math(&$arr_return, $count = 99999)
+function test_math(&$result, $count = 99999)
 {
-    $time_start = microtime(true);
-
+    $timeStart = microtime(true);
+    
     $mathFunctions = array("abs", "acos", "asin", "atan", "bindec", "floor", "exp", "sin", "tan", "pi", "is_finite", "is_nan", "sqrt");
     for ($i = 0; $i < $count; $i++) {
         foreach ($mathFunctions as $function) {
-            $r = call_user_func_array($function, array($i));
+            call_user_func_array($function, array($i));
         }
     }
-
-    $arr_return['benchmark']['math'] = timer_diff($time_start);
+    $result['benchmark']['math'] = timer_diff($timeStart);
 }
 
-function test_string(&$arr_return, $count = 99999)
+function test_string(&$result, $count = 99999)
 {
-    $time_start = microtime(true);
+    $timeStart = microtime(true);
     $stringFunctions = array("addslashes", "chunk_split", "metaphone", "strip_tags", "md5", "sha1", "strtoupper", "strtolower", "strrev", "strlen", "soundex", "ord");
 
     $string = 'the quick brown fox jumps over the lazy dog';
     for ($i = 0; $i < $count; $i++) {
         foreach ($stringFunctions as $function) {
-            $r = call_user_func_array($function, array($string));
+            call_user_func_array($function, array($string));
         }
     }
-    $arr_return['benchmark']['string'] = timer_diff($time_start);
+    $result['benchmark']['string'] = timer_diff($timeStart);
 }
 
-function test_loops(&$arr_return, $count = 999999)
+function test_loops(&$result, $count = 999999)
 {
-    $time_start = microtime(true);
-    for ($i = 0; $i < $count; ++$i)
-        ;
+    $timeStart = microtime(true);
+    for ($i = 0; $i < $count; ++$i);
     $i = 0;
     while ($i < $count) {
         ++$i;
     }
-
-    $arr_return['benchmark']['loops'] = timer_diff($time_start);
+    $result['benchmark']['loops'] = timer_diff($timeStart);
 }
 
-function test_ifelse(&$arr_return, $count = 999999)
+function test_ifelse(&$result, $count = 999999)
 {
-    $time_start = microtime(true);
+    $timeStart = microtime(true);
     for ($i = 0; $i < $count; $i++) {
         if ($i == -1) {
-
+            
         } elseif ($i == -2) {
-
+            
         } else if ($i == -3) {
-
+            
         }
     }
-    $arr_return['benchmark']['ifelse'] = timer_diff($time_start);
+    $result['benchmark']['ifelse'] = timer_diff($timeStart);
 }
 
-function test_mysql(&$arr_return, $arr_cfg)
+function test_mysql(&$result, $settings)
 {
+    $timeStart = microtime(true);
 
-    $time_start = microtime(true);
+    $link = mysqli_connect($settings['db.host'], $settings['db.user'], $settings['db.pw']);
+    $result['benchmark']['mysql']['connect'] = timer_diff($timeStart);
 
-    $link = mysqli_connect($arr_cfg['db.host'], $arr_cfg['db.user'], $arr_cfg['db.pw']);
-    $arr_return['benchmark']['mysql']['connect'] = timer_diff($time_start);
+    //$arr_return['sysinfo']['mysql_version'] = '';
 
-    // //$arr_return['sysinfo']['mysql_version'] = '';
+    mysqli_select_db($link, $settings['db.name']);
+    $result['benchmark']['mysql']['select_db'] = timer_diff($timeStart);
 
-    mysqli_select_db($link, $arr_cfg['db.name']);
-    $arr_return['benchmark']['mysql']['select_db'] = timer_diff($time_start);
-
-    $result = mysqli_query($link, 'SELECT VERSION() as version;');
-    $arr_row = mysqli_fetch_assoc($result);
-    $arr_return['sysinfo']['mysql_version'] = $arr_row['version'];
-    $arr_return['benchmark']['mysql']['query_version'] = timer_diff($time_start);
+    $dbResult = mysqli_query($link, 'SELECT VERSION() as version;');
+    $arr_row = mysqli_fetch_array($dbResult);
+    $result['sysinfo']['mysql_version'] = $arr_row['version'];
+    $result['benchmark']['mysql']['query_version'] = timer_diff($timeStart);
 
     $query = "SELECT BENCHMARK(1000000,ENCODE('hello','goodbye'));";
-    $result = mysqli_query($link, $query);
-    $arr_return['benchmark']['mysql']['query_benchmark'] = timer_diff($time_start);
+    $dbResult = mysqli_query($link, $query);
+    $result['benchmark']['mysql']['query_benchmark'] = timer_diff($timeStart);
 
     mysqli_close($link);
 
-    $arr_return['benchmark']['mysql']['total'] = timer_diff($time_start);
-
-    return $arr_return;
+    $result['benchmark']['mysql']['total'] = timer_diff($timeStart);
+    return $result;
 }
 
-function timer_diff($time_start)
+function timer_diff($timeStart)
 {
-    return number_format(microtime(true) - $time_start, 3);
+    return number_format(microtime(true) - $timeStart, 3);
 }
 
-function array_to_html($my_array)
+function array_to_html($array)
 {
-    $strReturn = '';
-    if (is_array($my_array)) {
-        $strReturn .= '<table>';
-        foreach ($my_array as $k => $v) {
-            $strReturn .= "\n<tr><td>";
-            $strReturn .= '<strong>' . htmlentities($k) . "</strong></td><td>";
-            $strReturn .= array_to_html($v);
-            $strReturn .= "</td></tr>";
+    $result = '';
+    if (is_array($array)) {
+        $result .= '<table>';
+        foreach ($array as $k => $v) {
+            $result .= "\n<tr><td>";
+            $result .= '<strong>' . htmlentities($k) . "</strong></td><td>";
+            $result .= array_to_html($v);
+            $result .= "</td></tr>";
         }
-        $strReturn .= "\n</table>";
+        $result .= "\n</table>";
     } else {
-        $strReturn = htmlentities($my_array);
+        $result = htmlentities($array);
     }
-    return $strReturn;
+    return $result;
 }
